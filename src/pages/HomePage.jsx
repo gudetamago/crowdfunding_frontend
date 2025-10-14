36243dbcd8f3a00ead2fd1193fe1a01ef20ff1d3
gudetamago <sharakael@yahoo.com>
2025-10-14 18:00:15 +0800

    Applied css to Homepage and added css to NavBar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { allCampaigns } from "../data";
 import CampaignCard from "../components/CampaignCard";
+import "./HomePage.css";
 
 function HomePage() {
 
   return (
-      <div>
+      <div id="campaign-list">
           {allCampaigns.map((campaignData, key) => {
               return <CampaignCard key={key} campaignData={campaignData} />;
           })}
