f46d007955088cfb580ef9bdd913a808052be47a
gudetamago <sharakael@yahoo.com>
2025-10-19 15:15:47 +0800

    Oops. Added logout function again, this time following proper instruction from class notes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,28 @@
 import { Link, Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/use-auth.js";
+// import { useNavigate } from "react-router-dom";
 
 import "./NavBar.css";
 
 function NavBar() {
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const {auth, setAuth} = useAuth();
 
-  // Check if user is logged in
-  const hasToken = window.localStorage.getItem('token');
+  // Check if user is logged in - this is a simpler way than the above
+  // const hasToken = window.localStorage.getItem('token');
 
   // Logout function
   const handleLogout = () => {
       // Remove token from localStorage
       window.localStorage.removeItem('token');
+      setAuth({ token: null });
 
       // Optional: Redirect to home page after logout
-      navigate('/');
+      // navigate('/');
 
       // Optional: Reload the page to reset any cached state
-      window.location.reload();
+      // window.location.reload();
   };
 
   return (
@@ -29,17 +32,17 @@ function NavBar() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
 
-            {/* Show Login link only if user is not logged in */}
-            {!hasToken && (<li><Link to="/login">Log In</Link></li>)}
-
-            {/* Show Logout link only if user is logged in */}
-            {hasToken && (
-                <li><button onClick={handleLogout} className="logout-btn">
-                    Log Out
-                </button></li>
+            {auth.token ? (
+                <li>
+                    <Link to="/" onClick={handleLogout}>
+                        Log Out
+                    </Link>
+                </li>
+                ) : (
+                <li>
+                    <Link to="/login">Log In</Link>
+                </li>
             )}
-
-
         </ul>
       </nav>
       <Outlet />
