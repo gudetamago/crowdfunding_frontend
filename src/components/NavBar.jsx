df82d3b46292e62e5087239c18267e84061b8279
gudetamago <sharakael@yahoo.com>
2025-10-19 14:25:23 +0800

    Added logout function and CSS just for the link/button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,45 @@
 import { Link, Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import "./NavBar.css";
 
 function NavBar() {
+
+  const navigate = useNavigate();
+
+  // Check if user is logged in
+  const hasToken = window.localStorage.getItem('token');
+
+  // Logout function
+  const handleLogout = () => {
+      // Remove token from localStorage
+      window.localStorage.removeItem('token');
+
+      // Optional: Redirect to home page after logout
+      navigate('/');
+
+      // Optional: Reload the page to reset any cached state
+      window.location.reload();
+  };
+
   return (
     <div>
       <nav>
         <ul>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Log In</Link></li>
             <li><Link to="/about">About</Link></li>
+
+            {/* Show Login link only if user is not logged in */}
+            {!hasToken && (<li><Link to="/login">Log In</Link></li>)}
+
+            {/* Show Logout link only if user is logged in */}
+            {hasToken && (
+                <li><button onClick={handleLogout} className="logout-btn">
+                    Log Out
+                </button></li>
+            )}
+
+
         </ul>
       </nav>
       <Outlet />
